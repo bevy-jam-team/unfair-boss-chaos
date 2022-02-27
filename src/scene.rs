f76f464dd0a9e41a372c37e258df99f0fbf8c2da
Isaak Eriksson <isaak.eriksson@protonmail.com>
2022-02-27 21:40:44 +0100

    Fix collision system

--- src/scene.rs
+++ src/scene.rs
@@ -4,40 +4,60 @@ use bevy_rapier2d::prelude::*;
 pub struct SetupScenePlugin;
 
 impl Plugin for SetupScenePlugin {
-    fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_camera_and_scene);
-    }
+	fn build(&self, app: &mut App) {
+		app.add_startup_system(spawn_camera_and_scene);
+	}
 }
 
 #[derive(Component)]
 pub struct CameraTag;
 
 /// Startup system. Spawns all the things that are necessary to render the scene
-fn spawn_camera_and_scene(mut commands: Commands) {
-    // camera
-    commands
-        .spawn_bundle(OrthographicCameraBundle::new_2d())
-        .insert(CameraTag);
+fn spawn_camera_and_scene(mut commands: Commands, rapier_parameters: Res<RapierConfiguration>) {
+	info!("SPAWN_CAMERA_AND_SCENE");
 
-    // test dummy rigidbody
-    commands
-        .spawn_bundle(SpriteBundle {
-            transform: Transform {
-                translation: Vec3::new(0.0, 100.0, 0.0),
-                ..Default::default()
-            },
-            sprite: Sprite {
-                color: Color::rgb(0.0, 0.0, 0.0),
-                custom_size: Some(Vec2::new(50.0, 10.0)),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .insert_bundle(RigidBodyBundle::default())
-        .insert_bundle(ColliderBundle {
-            position: [2.5 / 2.0, 0.5 / 2.0].into(),
-            ..Default::default()
-        });
+	// camera
+	commands
+		.spawn_bundle(OrthographicCameraBundle::new_2d())
+		.insert(CameraTag);
 
-    // TODO: spawn more level assets
+	// test dummy rigidbody
+	commands
+		.spawn_bundle(SpriteBundle {
+			sprite: Sprite {
+				color: Color::rgb(0.0, 0.0, 0.0),
+				custom_size: Some(Vec2::new(50.0, 10.0)),
+				..Default::default()
+			},
+			..Default::default()
+		})
+		.insert_bundle(RigidBodyBundle {
+			position: RigidBodyPosition {
+				position: Isometry::translation(0.0, 150.0 / rapier_parameters.scale),
+				..Default::default()
+			}
+			.into(),
+			forces: RigidBodyForces {
+				torque: 2.0,
+				..Default::default()
+			}
+			.into(),
+			damping: RigidBodyDamping {
+				linear_damping: 1.0,
+				angular_damping: 1.0,
+			}
+			.into(),
+			..Default::default()
+		})
+		.insert_bundle(ColliderBundle {
+			position: Vec2::ZERO.into(),
+			shape: ColliderShapeComponent(ColliderShape::cuboid(
+				50.0 / rapier_parameters.scale,
+				10.0 / rapier_parameters.scale,
+			)),
+			..Default::default()
+		})
+		.insert(ColliderPositionSync::Discrete);
+
+	// TODO: spawn more level assets
 }

0f5208e372d5b26053e1f354c793abca5a08457c
Isaak Eriksson <isaak.eriksson@protonmail.com>
2022-02-28 13:07:16 +0100

    Add physics to bullets

--- src/shooting.rs
+++ src/shooting.rs
@@ -1,4 +1,5 @@
-use bevy::prelude::*;
+use bevy::{math::Vec3Swizzles, prelude::*};
+use bevy_rapier2d::prelude::*;
 
 use crate::{input::MousePosition, player::Player};
 use bevy_inspector_egui::Inspectable;
@@ -13,15 +14,13 @@ impl Plugin for ShootingPlugin {
 				.with_system(check_for_shoot_event)
 				.label("check_for_shoot_event")
 				.with_system(shoot)
-				.label("shoot")
-				.with_system(move_bullets)
-				.label("move_bullets"),
+				.label("shoot"),
 		);
 	}
 }
 
 /// Values we might want to tweak and that are used to define specific properties of the entities.
-const BULLET_SPEED_VALUE: f32 = 300.0;
+const BULLET_FORCE_SCALE: f32 = 1000.0;
 
 /// used to check and trigger the shooting mechanic
 struct ShootEvent;
@@ -39,7 +38,7 @@ struct Speed {
 	value: f32,
 }
 
-#[derive(Inspectable, Component)]
+#[derive(Inspectable, Component, Clone)]
 struct Direction {
 	value: Vec2,
 }
@@ -55,25 +54,16 @@ struct BulletBundle {
 	direction: Direction,
 	#[bundle]
 	sprite: SpriteBundle,
+	#[bundle]
+	rigidbody: RigidBodyBundle,
+	#[bundle]
+	collider: ColliderBundle,
 }
 
 // SYSTEMS
 
 // The names of the systems are as expressive as possible in order to allow an easy understanding of
 // what they are doing
-
-/// System that moves the bullets according to their direction and speed (direction is calculated when the bullet is spawned)
-fn move_bullets(
-	mut bullets_query: Query<(&mut Transform, &Direction, &Speed), With<BulletTag>>,
-	time: Res<Time>,
-) {
-	for (mut bullet_transform, bullet_direction, bullet_speed) in bullets_query.iter_mut() {
-		bullet_transform.translation.x +=
-			bullet_direction.value.x * bullet_speed.value * time.delta_seconds();
-		bullet_transform.translation.y +=
-			bullet_direction.value.y * bullet_speed.value * time.delta_seconds();
-	}
-}
 
 /// System that checks if the mouse button has been pressed. If so, queues a new event to shoot a bullet
 fn check_for_shoot_event(
@@ -92,38 +82,63 @@ fn shoot(
 	mut ev_shoot_reader: EventReader<ShootEvent>,
 	mouse_info: Res<MousePosition>,
 	player_info: Query<&Transform, With<Player>>,
+	rapier_parameters: Res<RapierConfiguration>,
 ) {
 	let player_transform = player_info.single();
+	let bullet_offset = 1.0;
 
 	for _ in ev_shoot_reader.iter() {
-		commands.spawn_bundle(BulletBundle {
-			tag: BulletTag,
-			speed: Speed {
-				value: BULLET_SPEED_VALUE,
-			},
-			direction: Direction {
-				value: Vec2::new(
-					mouse_info.x - player_transform.translation.x,
-					mouse_info.y - player_transform.translation.y,
-				)
-				.normalize(),
-			},
-			sprite: SpriteBundle {
-				sprite: Sprite {
-					color: Color::rgb(0.75, 0.75, 0.75),
+		let direction = Direction {
+			value: Vec2::new(
+				mouse_info.x - player_transform.translation.x,
+				mouse_info.y - player_transform.translation.y,
+			)
+			.normalize(),
+		};
+		commands
+			.spawn_bundle(BulletBundle {
+				tag: BulletTag,
+				speed: Speed {
+					value: BULLET_FORCE_SCALE,
+				},
+				direction: direction.clone(),
+				sprite: SpriteBundle {
+					sprite: Sprite {
+						color: Color::rgb(0.75, 0.75, 0.75),
+						..Default::default()
+					},
+					transform: Transform {
+						translation: player_transform.translation,
+						scale: Vec3::new(15.0, 15.0, 0.0),
+						..Default::default()
+					},
 					..Default::default()
 				},
-				transform: Transform {
-					translation: Vec3::new(
-						player_transform.translation.x,
-						player_transform.translation.y,
-						0.0,
-					),
-					scale: Vec3::new(15.0, 15.0, 0.0),
+				rigidbody: RigidBodyBundle {
+					position: RigidBodyPosition {
+						position: (player_transform.translation.xy() / rapier_parameters.scale
+							+ direction.value * bullet_offset)
+							.into(),
+						..Default::default()
+					}
+					.into(),
+					forces: RigidBodyForces {
+						force: (direction.value * BULLET_FORCE_SCALE).into(),
+						..Default::default()
+					}
+					.into(),
 					..Default::default()
 				},
-				..Default::default()
-			},
-		});
+				collider: ColliderBundle {
+					flags: ColliderFlags {
+						// only ignore group 0, which should be player. NOTE: only works for player spawned bullets
+						collision_groups: InteractionGroups::new(u32::MAX, !0b0001),
+						..Default::default()
+					}
+					.into(),
+					..Default::default()
+				},
+			})
+			.insert(ColliderPositionSync::Discrete);
 	}
 }
